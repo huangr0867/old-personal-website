4f189ae73235a72b931aa5e653975e05dfd72a37
huangr0867 <rhuang53@gmail.com>
2023-03-26 12:59:42 -0400

    added dark/night mode

---
+++ index.js
@@ -0,0 +1,37 @@
+var today = new Date();
+var bg = document.getElementById("dark-mode-bg");
+var words = document.getElementsByClassName("dark-mode-words");
+var nav = document.getElementById("dark-mode-nav");
+var footer = document.getElementById("dark-mode-footer");
+var night = document.getElementById("night-mode");
+
+if (today.getHours() > 17) {
+    nightMode();
+}
+
+function nightMode (){
+        bg.style.backgroundColor = "#21252A";
+        footer.style.backgroundColor = "#383c44";
+        for (let word of words) {
+            word.style.color = "white";
+        }
+        nav.classList.remove("navbar-light");
+        nav.classList.remove("bg-light");
+        nav.classList.add("navbar-dark");
+        nav.classList.add("bg-dark");
+        night.onclick=lightMode;
+    
+}
+
+function lightMode (){
+    bg.style.backgroundColor = "white";
+    footer.style.backgroundColor = "white";
+    for (let word of words) {
+        word.style.color = "black";
+    }
+    nav.classList.remove("navbar-dark");
+    nav.classList.remove("bg-dark");
+    nav.classList.add("navbar-light");
+    nav.classList.add("bg-light");
+    night.onclick=nightMode;
+}
