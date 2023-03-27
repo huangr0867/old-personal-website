e641310836d93a9cb658a6c425287c296655bac4
huangr0867 <rhuang53@gmail.com>
2023-03-27 15:02:44 -0400

    hover

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ var footer = document.getElementById("dark-mode-footer");
 var night = document.getElementById("night-mode");
 var box1 = document.getElementsByClassName("box");
 var boxPublication = document.getElementsByClassName("box-publication");
-var nightIcon = document.getElementById("night-icon")
+var boxEXP = document.getElementsByClassName("box-exp");
+var nightIcon = document.getElementById("night-icon");
 
 if (today.getHours() > 17) {
     nightMode();
@@ -18,7 +19,22 @@ function nightMode (){
         bg.style.backgroundColor = "#21252A";
         footer.style.backgroundColor = "#383c44";
         for (let box of box1) {
-            box.style.borderColor = "white"
+            box.style.borderColor = "white";
+            box.addEventListener("mouseenter", function( event ) {   
+                event.target.style.borderColor = "transparent";
+              }, false);
+              box.addEventListener("mouseleave", function( event ) {   
+                event.target.style.borderColor = "white";
+              }, false);
+        }
+        for (let box of boxEXP) {
+            box.style.borderColor = "white";
+            box.addEventListener("mouseenter", function( event ) {   
+                event.target.style.borderColor = "transparent";
+              }, false);
+              box.addEventListener("mouseleave", function( event ) {   
+                event.target.style.borderColor = "white";
+              }, false);
         }
         for (let box of boxPublication) {
             box.style.borderColor = "white"
@@ -31,6 +47,8 @@ function nightMode (){
         nav.classList.add("navbar-dark");
         nav.classList.add("bg-dark");
         night.onclick=lightMode;
+
+        
     
 }
 
@@ -38,7 +56,22 @@ function lightMode (){
     bg.style.backgroundColor = "white";
     footer.style.backgroundColor = "#fafafa"
     for (let box of box1) {
-        box.style.borderColor = "black"
+        box.style.borderColor = "black";
+        box.addEventListener("mouseenter", function( event ) {   
+            event.target.style.borderColor = "transparent";
+          }, false);
+          box.addEventListener("mouseleave", function( event ) {   
+            event.target.style.borderColor = "black";
+          }, false);
+    }
+    for (let box of boxEXP) {
+        box.style.borderColor = "black";
+        box.addEventListener("mouseenter", function( event ) {   
+            event.target.style.borderColor = "transparent";
+          }, false);
+          box.addEventListener("mouseleave", function( event ) {   
+            event.target.style.borderColor = "black";
+          }, false);
     }
     for (let box of boxPublication) {
         box.style.borderColor = "black"
